fc1381e98c67aeb00ebab1d5ac071d73b58e187f
Stefanie Stoppel <s.stoppel@gmx.de>
2020-12-09 16:01:09 +0100

    feat: add early stopping criterion

--- src/inferface/run.py
+++ src/inferface/run.py
@@ -3,12 +3,15 @@ import os
 import pytorch_lightning as pl
 
 from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks import EarlyStopping
+
+from inferface.config import LossNames
 from inferface.dataset.fairface_dataloader_factory import FairFaceDataModule
 from inferface.network import AgeGenderRaceClassifier
 from src import ROOT_DIR
 
 
-def train():
+def run():
     # init model
     lr = 1e-3
     dropout = 0.3
@@ -19,10 +22,19 @@ def train():
     test_csv_path = '/home/steffi/dev/independent_study/fairface_margin_025/embeddings/val_labels_and_embeddings.csv'
     fairface_data_module = FairFaceDataModule(train_csv_path, test_csv_path)
 
+    # Early stopping based on val_loss
+    early_stop_callback = EarlyStopping(
+        monitor=LossNames.VAL_LOSS_TOTAL.value,
+        min_delta=0.00,
+        patience=3,
+        verbose=False,
+        mode='min'
+    )
+
     # most basic trainer, uses good defaults (auto-tensorboard, checkpoints, logs, and more)
-    trainer = pl.Trainer(gpus=1, logger=tb_logger)
+    trainer = pl.Trainer(gpus=1, logger=tb_logger, callbacks=[early_stop_callback])
     trainer.fit(model, datamodule=fairface_data_module)
 
 
 if __name__ == "__main__":
-    train()
+    run()
